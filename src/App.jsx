a777d788add7b8e4b6909609355bec20000d6e5d
afker please <afker2020@gmail.com>
2023-09-15 12:31:10 +0200

    phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
-import { useState, useReducer, useContext } from "react";
+import { useReducer, useContext, createContext } from "react";
 
 import "./App.css";
 
+import FormDepenses from "./components/FormDepenses";
+
+const initalState = {
+  dépenses: [],
+  total: 0,
+  categorieDépense: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      const { categorie } = action.payload;
+      const updateCategorie = { ...state.categorieDépense };
+      updateCategorie[categorie] =
+        (updateCategorie[categorie] || 0) + +action.payload.prix;
+      return {
+        ...state,
+        dépenses: [...state.dépenses, action.payload],
+        total: state.total + +action.payload.prix,
+        categorieDépense: updateCategorie,
+      };
+  }
+};
+
 function App() {
-  return <></>;
+  const [state, dispatch] = useReducer(reducer, initalState);
+  console.log(state.categorieDépense);
+  return (
+    <>
+      <FormDepenses dispatch={dispatch} />
+
+      <h6> {state.total} </h6>
+      {Object.values(state.categorieDépense).map((category, index) => (
+        <h1 key={index}> {category} </h1>
+      ))}
+      {Object.keys(state.categorieDépense).map((category, index) => (
+        <h1 key={index}> {category} </h1>
+      ))}
+
+      {state.dépenses.map((dep, index) => (
+        <div style={{ border: "1px solid black" }} key={index}>
+          <h1> {dep.description} </h1>
+          <h2>{dep.prix} </h2>
+          <p>{dep.categorie}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/FormDepenses.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function FormDepenses({ dispatch }) {
+  const categories = [
+    "Alimentation",
+    "Logement",
+    "Transport",
+    "Divertissement",
+    "Santé",
+    "Éducation",
+    "Autres",
+  ];
+  const [selectedCategory, setSelectedCategory] = useState("Alimentation");
+  const [description, setDescription] = useState("");
+  const [prix, setprix] = useState("");
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSelectedCategory(value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "add",
+      payload: {
+        categorie: selectedCategory,
+        description,
+        prix,
+      },
+    });
+    setDescription("");
+    setprix("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label>
+        Description
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+        />
+      </label>
+      <label htmlFor="">
+        Prix
+        <input
+          value={prix}
+          onChange={(e) => setprix(e.target.value)}
+          type="number"
+        />
+      </label>
+      <select
+        name={"category"}
+        value={selectedCategory}
+        onChange={handleChange}
+      >
+        {categories.map((category, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <button type="submit"> Ajouter</button>
+    </form>
+  );
+}
+
+export default FormDepenses;
