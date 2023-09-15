a6402b60464d388536de08b5a1945c7b6f64ce6a
afker please <afker2020@gmail.com>
2023-09-15 17:09:36 +0200

    lastPush

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-const theme = {
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: true,
-  },
-};
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
